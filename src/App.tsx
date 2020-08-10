2ae5d43400e571d6a45976290f5954a573d633bc
hammadasad22 <hammadasad190@gmail.com>
2020-08-10 13:11:05 +0500

    quizapp completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
+import { getQuizDetails } from './services/quiz-services'
+import { QuestionType } from './Types/quiz_types';
+import QuestionCard from './components/QuestionCard'
 
 function App() {
+
+  let [quiz, setquiz] = useState<QuestionType[]>([])
+  let [currentStep, setcurrentStep] = useState(0)
+  let [score, setScore] = useState(0)
+  let [showResult, setShowResult] = useState(false)
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const questions: QuestionType[] = await getQuizDetails(5, 'easy');
+      setquiz(questions)
+    }
+    fetchData()
+  }, [])
+  const handleSubmit = (e: React.FormEvent<EventTarget>, userAns: string) => {
+    const currentQuestion: QuestionType = quiz[currentStep];
+    console.log(`correct answer is : ${currentQuestion.correct_answer} user selection is: ${userAns}`)
+
+
+    if (userAns === currentQuestion.correct_answer) {
+      setScore(++score)
+    }
+
+    e.preventDefault();
+    if (currentStep !== quiz.length - 1)
+      setcurrentStep(++currentStep);
+    else {
+      setShowResult(true);
+      
+
+    }
+  }
+  if (!quiz.length)
+    return <div className="loading"><img src="https://media3.giphy.com/media/WiIuC6fAOoXD2/200w.webp?cid=ecf05e47d8xx23xazrpodvfn83dtaen94jldn5tbnkddu99b&rid=200w.webp" alt="Loading"/></div>
+    if(showResult){
+      return (
+      <div className="question-container result-container">
+        <h2>Result</h2>
+  
+        <p className="result-text">
+          You final score is 
+            <b> {score}</b> out of <b>{quiz.length}</b>
+        </p>
+      </div>
+      )
+      
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        You answered {currentStep} out of {quiz.length} questions
+      </p>
+
+      <QuestionCard
+        options={quiz[currentStep].option}
+        question={quiz[currentStep].question.replace(/&quot;|&#039;/g, "'")}
+        callback={handleSubmit}
+      />
     </div>
   );
 }
---
+++ src/Types/quiz_types.ts
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export type QuizType = {
+    category: string
+    correct_answer: string
+    difficulty: string
+    incorrect_answers: string[]
+    question: string
+    type: string
+}
+
+export type QuestionType = {
+    question: string
+    answer: string
+    option: string[]
+    correct_answer: string
+}
+
+export type QuestionPropsType = {
+    question: string
+    options: string[]
+    callback: (e: React.FormEvent<EventTarget>, ans:string) => void
+
+}
